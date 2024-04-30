a9d62caadc7bf8c6fd43ea6d49b9795d75f5940a
Daniel Corbacho Anes <d.corbacho.2023@alumnos.urjc.es>
2024-04-30 14:03:49 +0200

    practica

---
+++ scripts/HelloWorld.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=> {
+	var boton3 = document.getElementById('boton3');
+			boton3.addEventListener('click', () => {
+				alert("Si pulsa aceptar, Pablo es re-gay :)");
+			});
+});
+
